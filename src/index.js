3526470ffa1c542718d11e91fdf95900177dfcfe
Simon <simonsch.tz@gmail.com>
2020-02-27 13:13:24 +0100

    Stop game when won

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ class Square extends React.Component {
 }
 
 ///////////////////////////////////////////
-//              UPPER LEVEL              //
+//               MID LEVEL               //
 ///////////////////////////////////////////
 class Board extends React.Component {
   constructor(props) {
@@ -41,7 +41,6 @@ class Board extends React.Component {
       { i: 6, occupiedBy: "" },
       { i: 7, occupiedBy: "" },
       { i: 8, occupiedBy: "" }],
-      winner: ""
     }
   }
 
@@ -49,59 +48,58 @@ class Board extends React.Component {
   //if occupiedBy = "" on index X  setState occupiedBy current player + then switch player
 
   playerMove = i => {
-    const squares = this.state.squares
-    const newState = squares.map(el => {
-      if (el.i === i && el.occupiedBy === "") {
-        el.occupiedBy = this.state.nextPlayer
-        this.switchPlayer()
+    if (!this.props.gameWon) {
+      console.log(this.props)
+      const squares = this.state.squares
+      const newState = squares.map(el => {
+        if (el.i === i && el.occupiedBy === "") {
+          el.occupiedBy = this.state.nextPlayer
+          this.switchPlayer()
+        }
+        return el
+      })
+      this.setState({ squares: newState })
+      //Winning conditions horizontal
+      const winner = this.state.nextPlayer
+      if ((squares[0].occupiedBy === squares[1].occupiedBy) && (squares[0].occupiedBy === squares[2].occupiedBy) && (squares[0].occupiedBy !== "")) {
+        this.props.win(winner)
       }
-      return el
-    })
-    this.setState({ squares: newState })
-    //Winning conditions horizontal
-    const winner = this.state.nextPlayer
-    if ((squares[0].occupiedBy === squares[1].occupiedBy) && (squares[0].occupiedBy === squares[2].occupiedBy) && (squares[0].occupiedBy !== "")) {
-      this.props.win(winner)
+      if ((squares[3].occupiedBy === squares[4].occupiedBy) && (squares[3].occupiedBy === squares[5].occupiedBy) && (squares[3].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
+      if ((squares[6].occupiedBy === squares[7].occupiedBy) && (squares[6].occupiedBy === squares[8].occupiedBy) && (squares[6].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
+      //Winning conditions vertical
+      if ((squares[0].occupiedBy === squares[3].occupiedBy) && (squares[0].occupiedBy === squares[6].occupiedBy) && (squares[0].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
+      if ((squares[1].occupiedBy === squares[4].occupiedBy) && (squares[1].occupiedBy === squares[7].occupiedBy) && (squares[1].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
+      if ((squares[2].occupiedBy === squares[5].occupiedBy) && (squares[2].occupiedBy === squares[8].occupiedBy) && (squares[2].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
+      //Winning conditions diagonal
+      if ((squares[0].occupiedBy === squares[4].occupiedBy) && (squares[0].occupiedBy === squares[8].occupiedBy) && (squares[0].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
+      if ((squares[2].occupiedBy === squares[4].occupiedBy) && (squares[2].occupiedBy === squares[6].occupiedBy) && (squares[2].occupiedBy !== "")) {
+        this.props.win(winner)
+      }
     }
-    if ((squares[3].occupiedBy === squares[4].occupiedBy) && (squares[3].occupiedBy === squares[5].occupiedBy) && (squares[3].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-    if ((squares[6].occupiedBy === squares[7].occupiedBy) && (squares[6].occupiedBy === squares[8].occupiedBy) && (squares[6].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-    //Winning conditions vertical
-    if ((squares[0].occupiedBy === squares[3].occupiedBy) && (squares[0].occupiedBy === squares[6].occupiedBy) && (squares[0].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-    if ((squares[1].occupiedBy === squares[4].occupiedBy) && (squares[1].occupiedBy === squares[7].occupiedBy) && (squares[1].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-    if ((squares[2].occupiedBy === squares[5].occupiedBy) && (squares[2].occupiedBy === squares[8].occupiedBy) && (squares[2].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-    //Winning conditions diagonal
-    if ((squares[0].occupiedBy === squares[4].occupiedBy) && (squares[0].occupiedBy === squares[8].occupiedBy) && (squares[0].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-    if ((squares[2].occupiedBy === squares[4].occupiedBy) && (squares[2].occupiedBy === squares[6].occupiedBy) && (squares[2].occupiedBy !== "")) {
-      this.props.win(winner)
-    }
-
   }
 
   switchPlayer = () => {
     if (this.state.nextPlayer === "X") {
       this.setState({ nextPlayer: "0" })
+      this.props.nextPlayer("O")
     } else {
       this.setState({ nextPlayer: "X" })
+      this.props.nextPlayer("X")
     }
   }
 
-  // win = winner => {
-  //   if (this.state.winner === "") {
-  //     this.setState({ winner: winner + " won the game!" })
-  //   }
-  // }
 
   renderSquare(i) {
     return (
@@ -112,11 +110,10 @@ class Board extends React.Component {
 
 
   render() {
-    const status = "Next player: " + this.state.nextPlayer;
 
     return (
       <React.Fragment>
-        <div className="status h2 text-center">{status}</div>
+        <div className="status h2 text-center">{/*to do dunno*/}</div>
         <div className="board">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -132,29 +129,38 @@ class Board extends React.Component {
     );
   }
 }
+
+///////////////////////////////////////////
+//              Upper LEVEL              //
+///////////////////////////////////////////
 class Game extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      winner: ""
+      message: "Next player: X",
+      gameWon: false
     }
 
   }
   win = (winner) => {
-    if (this.state.winner === "") {
-      this.setState({ winner: winner + " won the game!" })
-    }
+    this.setState({ message: winner + " won the game!" })
+    this.setState({ gameWon: true })
   }
 
+  nextPlayer = (player) => {
+    this.setState({ message: "Next player: " + player })
+
+
+  }
   render() {
     return (
       <article className="game container mt-5">
         <section className="row">
           <div className="col-sm-8 game-board">
-            <Board win={this.win} />
+            <Board win={this.win} nextPlayer={this.nextPlayer} gameWon={this.state.gameWon} />
           </div>
           <div className="col-sm-4 game-info">
-            <p className="h2">{this.state.winner}</p>
+            <p className="h2">{this.state.message}</p>
             <ul className="nav nav-pills flex-column">
               {/* TODO */}
             </ul>
